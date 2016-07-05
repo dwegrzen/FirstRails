48090d759fce649c28053645ab6a6edb8c306e6f
dwegrzen <dwegrzen@gmail.com>
2016-07-05 17:28:09 -0400

    adventure requirements completed I think

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,18 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-  get '/(:name)' => 'welcome#name'
-  get 'lorem/(:type)' => 'welcome#loremgen'
+  # get 'products/:id' => 'catalog#view'
+  get '/:name' => 'welcome#name'
+  get 'lorem/:type/(:paragraph)' => 'welcome#loremgen'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-      # resources :welcome
 
   # Example resource route with options:
   #   resources :products do
